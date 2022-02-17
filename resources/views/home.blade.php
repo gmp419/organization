edd4d84d2320f1c59f205004c7434952382ce12e
gaurang <patel7483g@gmail.com>
2022-02-17 16:04:25 -0400

    Starting project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+        <div class="card">
+            <h5 class="card-header">Home</h5>
+            <div class="card-body"> 
+                <h5 class="card-title">
+                <p class="card-text">
+                    Welcome to the dashboard!
+                </p>
+                </h5>
+            </div>
+            </div>  
+        </div>
+    </div>
+@endsection
